4143fb2bbec72558246489f47c30c26451075e50
willys123 <willysimatupang137@gmail.com>
2023-12-05 15:12:10 +0700

    fix:metatag image

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
       "Empowering Success through Innovative Software Solutions at Willyam Software House. Elevate your digital journey with our expert software development services. From custom applications to cutting-edge solutions, we blend creativity and technology for unparalleled results. Partner with us for a seamless integration of innovation and excellence. Unlock the potential of your business with Willyam Software House in Batam, Indonesia.",
     images: [
       {
-        url: "https://unsplash.com/photos/people-sitting-at-the-table-zBfBXHCaLmk",
+        url: "https://images.unsplash.com/photo-1612296727716-d6c69d2a2cbb?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ],
   },
